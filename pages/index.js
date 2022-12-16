cacebb78aefdc3fd7627d812f001ca1c2077fbea
Seif Ashraf <seif_fire2@hotmail.com>
2022-12-16 19:35:38 +0200

    application back to init

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,13 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   
-  const [movies, setMovies] = useState([])
 
-  const instance = axios.create({
-    baseURL: '',
-    timeout: 1000,
-    headers: {'X-Custom-Header': 'foobar'}
-    });
-
-  sendRequest = (title) => {
-
-
-    // const res = axios.get("https://movie-database-imdb-alternative.p.rapidapi.com/");
-    
-  }
-
-  useEffect(() => {
-    sendRequest('lord')
-  })
 
   return (
     <div className={styles.container}>
-      {
-        movies.map(movie => <p key={movie.id}>{movie.title}</p>)
-      }
+     
     </div>
   )
 }

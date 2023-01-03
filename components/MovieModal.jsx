78eb144b01d2be708014b393138fdc1791a1b808
Seif Ashraf <seif_fire2@hotmail.com>
2023-01-03 17:37:14 +0200

    deployment trial

--- components/MovieModal.jsx
+++ components/MovieModal.jsx
@@ -46,7 +46,6 @@ const MovieModal = ({ modalMovie }) => {
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">Save changes</button>
                     </div>
                     </div>
                 </div>
